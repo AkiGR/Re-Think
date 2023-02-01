213c9017d1b28e3b95cdc47f083180f6007c2a20
hanauchiakiya <hanauchi.aki@gmail.com>
2023-02-01 10:27:46 +0900

    linaria install

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+
+export const Sideber : FC = () => {
+  return (
+    <a>
+      Sideber
+    </a>
+  );
+}
---
+++ src/components/sideber/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Sideber : FC = () => {
+  return (
+    <a>
+      Sideber
+    </a>
+  );
+}
