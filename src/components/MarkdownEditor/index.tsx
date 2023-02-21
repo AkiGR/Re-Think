1d4069136f379deea8102739e5a30d736314e477
hanauchiakiya <hanauchi.aki@gmail.com>
2023-02-21 16:01:21 +0900

    Acquire h1 of the created note and display it modally

--- src/components/MarkdownEditor/index.tsx
+++ src/components/MarkdownEditor/index.tsx
@@ -1,10 +1,13 @@
-import { FC, useState } from 'react';
-import SimpleMDE , { SimpleMDEReactProps } from 'react-simplemde-editor';
+import { FC, useState , useRef, useEffect} from 'react';
+import SimpleMDE , {SimpleMDEReactProps } from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import 'github-markdown-css';
 import { styled } from '@stitches/react';
 import './style.css';
 import { Note } from '../../types';
+import Modal from 'react-modal';
+import { AiOutlineClose } from 'react-icons/ai';
+import { authPlugins } from 'mysql2';
 
 type Props = {
   folderId : number;
@@ -32,23 +35,100 @@ const StyledH1 = styled('h1', {
   padding : '10px',
 });
 
+const StyledTitle = styled('h1', {
+  marginTop : '20px',
+  zIndex : '1000',
+  textAlign : 'center',
+  color : '#ff4500',
+  fontSize : '5rem',
+  padding : '10px',
+});
+
+
+const Styledbutton = styled('div', {
+  marginTop : '20px',
+  zIndex : '1000',
+  textAlign : 'center',
+  color : 'rgb(86,87,79)',
+  fontSize : '2rem',
+  padding : '10px',
+  '&:hover ' : {
+    color : '#4682b4',
+  },
+});
+
+const StyledItemRight = styled('div' , {
+  textAlign : 'right',
+  fontSize : '3rem',
+});
+
+const StyledAnswerText = styled('div', {
+  textAlign : 'center',
+  marginTop : '10%',
+  fontSize : '2rem',
+});
+
 const options : SimpleMDEReactProps['options'] = {
   previewClass : 'markdown-body',
   minHeight : "590px",
 }
 
+const InternalWrapper = styled('div', {});
+
 export const MarkdownEditor : FC<Props> = ( { folderId , menuName } ) => {
+  const [ isOpen , setIsOpen ] = useState(true);
   const [ noteValue , setNoteValue ] = useState<string>('');
   const [ Notes , setNotes ] = useState<Note[]>([]);
+  const [heading, setHeading] = useState('');
+  const [content, setContent] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
+  const handleClick = ()  => {
+    setIsOpen((c) => !c);
+  }
+
+  Modal.setAppElement('#root');
   const handleSaveNote = (value : string) => {
     setNoteValue(value);
-    const newNote : Note = { folderId : folderId , name : menuName , body : noteValue};
-    setNotes((curNote) => curNote.concat(newNote))
-  }
+    console.log(noteValue);
+    
+  };
+
+  useEffect(() => {
+    const text = noteValue ? noteValue.valueOf() : '';
+    const regex = /^#\s(.+)/gm;
+    // const regex = /<p>([^<]+)<\/p>/i;
+    const match = regex.exec(text);
+    if (match) {
+      setHeading(match[1]);
+    } else {
+      setHeading('');
+    }
+  });
+
+  useEffect(() => {
+    const text = noteValue ? noteValue.valueOf() : '';
+    const regex = /^#\s.*\n\n(.+)/s;
+    const match = regex.exec(text);
+    if (match) {
+      setContent(match[1]);
+    } else {
+      setContent('');
+    }
+  });
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+
   return (
     <Wrapper>
-      <button> Test!! </button>
+      <button onClick={openModal}> Test!! </button>
       
       <StyledH1>Markdown Editor</StyledH1>
         <SimpleMDE
@@ -56,6 +136,27 @@ export const MarkdownEditor : FC<Props> = ( { folderId , menuName } ) => {
           options={options} 
           onChange={handleSaveNote} 
         />
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <StyledItemRight>
+          <AiOutlineClose onClick={closeModal} />
+        </StyledItemRight>
+
+        <StyledTitle>{heading}</StyledTitle>
+
+        <Styledbutton>
+            { isOpen ? <button onClick={handleClick} >答え合わせ</button> : <button onClick={handleClick} >答え合わせ</button> }
+        </Styledbutton>
+
+      <InternalWrapper
+        css ={{ display : isOpen ? "none" : "block", }}
+      >
+        <StyledAnswerText>
+          <div>{content}</div>
+        </StyledAnswerText>
+      </InternalWrapper>
+      </Modal>
+
     </Wrapper>
   )
 }
